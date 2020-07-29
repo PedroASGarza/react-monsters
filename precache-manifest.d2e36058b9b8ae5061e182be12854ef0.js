46c4d37d15d44a2d9a30e6ca4bc377bd3c31c3e2
Pedro <pedro.asgarza@gmail.com>
2020-07-28 22:18:48 -0500

    Updates

---
+++ precache-manifest.d2e36058b9b8ae5061e182be12854ef0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3bf374b1127da12165afa453c23c9c62",
+    "url": "/react-monsters/index.html"
+  },
+  {
+    "revision": "879f565f3f26a03cdef0",
+    "url": "/react-monsters/static/css/main.c6485797.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/react-monsters/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-monsters/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "879f565f3f26a03cdef0",
+    "url": "/react-monsters/static/js/main.d63400da.chunk.js"
+  },
+  {
+    "revision": "9e48f53adff59196fe9e",
+    "url": "/react-monsters/static/js/runtime-main.265b368b.js"
+  }
+]);
